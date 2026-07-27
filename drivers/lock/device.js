52d518b2fbbf3c3929d2845615fe3d5c68178740
Bjarne Riis <bjarne@briis.com>
2026-07-27 08:22:33 +0200

    Added Battery Low functionality

--- drivers/lock/device.js
+++ drivers/lock/device.js
@@ -3,6 +3,7 @@
 const { OAuth2Device } = require('homey-oauth2app');
 
 const POLL_INTERVAL = 60 * 1000;
+const BATTERY_LOW_THRESHOLD = 20;
 
 // TTLock `lock/queryOpenState` state: 0 = locked, 1 = unlocked, 2 = unknown
 const STATE_LOCKED = 0;
@@ -11,6 +12,10 @@ const STATE_UNLOCKED = 1;
 module.exports = class TTLockDevice extends OAuth2Device {
 
   async onOAuth2Init() {
+    if (!this.hasCapability('alarm_battery')) {
+      await this.addCapability('alarm_battery');
+    }
+
     this.registerCapabilityListener('locked', this.onCapabilityLocked.bind(this));
 
     await this.syncState().catch((err) => this.error('Failed to fetch initial lock state', err));
@@ -49,6 +54,7 @@ module.exports = class TTLockDevice extends OAuth2Device {
 
     if (typeof detail.electricQuantity === 'number') {
       await this.setCapabilityValue('measure_battery', detail.electricQuantity);
+      await this.setCapabilityValue('alarm_battery', detail.electricQuantity < BATTERY_LOW_THRESHOLD);
     }
   }
 
